eb6db4f9622caa426e74206eb4571f3b0a0d2abb
clearfeebaby <97785638+clearfeebaby@users.noreply.github.com>
2022-06-28 16:38:57 +0200

    Add tailvote and Workflow status

--- voting.js
+++ voting.js
@@ -11,7 +11,6 @@ contract('Voting', accounts => {
   const proposal1 = 'proposal1';
   const proposal2 = 'proposal2';
   const proposal3 = 'proposal3';
-  const winningProposal = 'winningProposal';
 
   let votingInstance;
 
@@ -93,10 +92,12 @@ contract('Voting', accounts => {
     });
   });
 
+  /*= ========= addProposal ========== */
+
   describe('Test addProposal', () => {
     beforeEach(async () => {
       votingInstance = await Voting.new({ from: owner });
-      votingInstance.addVoter(voter1, { from: owner });
+      await votingInstance.addVoter(voter1, { from: owner });
       await votingInstance.startProposalsRegistering({ from: owner });
     });
 
@@ -126,10 +127,17 @@ contract('Voting', accounts => {
       await votingInstance.addProposal(proposal1, {
         from: voter1,
       });
+      await votingInstance.addProposal(proposal2, {
+        from: voter1,
+      });
       const proposal = await votingInstance.getOneProposal(0, {
         from: voter1,
       });
+      const proposalBis = await votingInstance.getOneProposal(1, {
+        from: voter1,
+      });
       expect(proposal.description).to.be.equal(proposal1);
+      expect(proposalBis.description).to.be.equal(proposal2);
     });
 
     it('Proposal correct count', async () => {
@@ -151,6 +159,8 @@ contract('Voting', accounts => {
       });
     });
   });
+
+  /*= ========= setVote ========== */
 
   describe('test setVote', () => {
     beforeEach(async () => {
@@ -219,6 +229,135 @@ contract('Voting', accounts => {
         voter: voter1,
         proposalId: new BN(0),
       });
+    });
+  });
+
+  /*= ========= WorkflowStatus changes ========== */
+
+  describe('Test workflowstatus changes', function() {
+    before(async function() {
+      votingInstance = await Voting.new({ from: owner });
+    });
+
+    it('Change from RegisteringVoters to ProposalsRegistrationStarted + event', async () => {
+      const event = await votingInstance.startProposalsRegistering();
+      const status = await votingInstance.workflowStatus();
+      expect(new BN(status)).to.be.bignumber.equal(
+        new BN(Voting.WorkflowStatus.ProposalsRegistrationStarted)
+      );
+      expectEvent(event, 'WorkflowStatusChange', {
+        previousStatus: '0',
+        newStatus: '1',
+      });
+    });
+
+    it('Change from ProposalsRegistrationStarted to ProposalsRegistrationEnded + event', async () => {
+      const event = await votingInstance.endProposalsRegistering();
+      const status = await votingInstance.workflowStatus();
+      expect(new BN(status)).to.be.bignumber.equal(
+        new BN(Voting.WorkflowStatus.ProposalsRegistrationEnded)
+      );
+      expectEvent(event, 'WorkflowStatusChange', {
+        previousStatus: '1',
+        newStatus: '2',
+      });
+    });
+
+    it('Change from ProposalsRegistrationEnded to VotingSessionStarted + event', async () => {
+      const event = await votingInstance.startVotingSession();
+      const status = await votingInstance.workflowStatus();
+      expect(new BN(status)).to.be.bignumber.equal(
+        new BN(Voting.WorkflowStatus.VotingSessionStarted)
+      );
+      expectEvent(event, 'WorkflowStatusChange', {
+        previousStatus: '2',
+        newStatus: '3',
+      });
+    });
+
+    it('Change from VotingSessionStarted to VotingSessionEnded + event', async () => {
+      const event = await votingInstance.endVotingSession();
+      const status = await votingInstance.workflowStatus();
+      expect(new BN(status)).to.be.bignumber.equal(
+        new BN(Voting.WorkflowStatus.VotingSessionEnded)
+      );
+      expectEvent(event, 'WorkflowStatusChange', {
+        previousStatus: '3',
+        newStatus: '4',
+      });
+    });
+
+    it('Change from VotingSessionEnded to VotesTallied + event', async () => {
+      const event = await votingInstance.tallyVotes();
+      const status = await votingInstance.workflowStatus();
+      expect(new BN(status)).to.be.bignumber.equal(
+        new BN(Voting.WorkflowStatus.VotesTallied)
+      );
+      expectEvent(event, 'WorkflowStatusChange', {
+        previousStatus: '4',
+        newStatus: '5',
+      });
+    });
+  });
+
+  /*= ========= tallyVotes ========== */
+
+  describe('test tallyVotes', () => {
+    beforeEach(async () => {
+      votingInstance = await Voting.new({ from: owner });
+      await votingInstance.addVoter(voter1, { from: owner });
+      await votingInstance.addVoter(voter2, { from: owner });
+      await votingInstance.addVoter(voter3, { from: owner });
+
+      await votingInstance.startProposalsRegistering({ from: owner });
+
+      await votingInstance.addProposal(proposal1, {
+        from: voter1,
+      });
+      await votingInstance.addProposal(proposal2, {
+        from: voter2,
+      });
+      await votingInstance.addProposal(proposal3, {
+        from: voter2,
+      });
+
+      await votingInstance.endProposalsRegistering({ from: owner });
+      await votingInstance.startVotingSession({ from: owner });
+
+      await votingInstance.setVote(2, { from: voter1 });
+      await votingInstance.setVote(2, { from: voter2 });
+      await votingInstance.setVote(1, { from: voter3 });
+    });
+
+    it('Revert because not owner', async () => {
+      await expectRevert(
+        votingInstance.tallyVotes({ from: voter1 }),
+        'Ownable: caller is not the owner'
+      );
+    });
+
+    it('Revert because voting session is not ending', async () => {
+      await expectRevert(
+        votingInstance.tallyVotes({ from: owner }),
+        'Current status is not voting session ended'
+      );
+    });
+
+    it('Display the winning proposal', async () => {
+      await votingInstance.endVotingSession({ from: owner });
+      await votingInstance.tallyVotes({ from: owner });
+      const winningProposalFromVariable = await votingInstance.winningProposalID.call();
+      expect(new BN(winningProposalFromVariable)).to.be.bignumber.equal(
+        new BN(2)
+      );
+
+      const winningProposalFromGetter = await votingInstance.getOneProposal(2, {
+        from: voter1,
+      });
+      expect(winningProposalFromGetter.description).to.be.equal(proposal3);
+      expect(new BN(winningProposalFromGetter.voteCount)).to.be.bignumber.equal(
+        new BN(2)
+      );
     });
   });
 });
